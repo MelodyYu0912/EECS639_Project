d74bf3a12a18e665bb7a12054ea3fc2723916cda
Melody Yu <x864y008@ku.edu>
2018-11-17 20:46:46 -0600

    test file updated

--- test.m
+++ test.m
@@ -1,4 +1,71 @@
 clear, clc;
 
+<<<<<<< Updated upstream
 x = [-2 -27; 0 -1; 1 0];
 Vandermonde_Interpolation(x)
+||||||| merged common ancestors
+x = [-2 -27; 0 -1; 1 0]
+
+vandermonde(x)
+=======
+% Test ¢ñ: f(x) = exp(x^2), x ¡Ê[0.6,1]; 5 equally spaced points:
+syms x;
+f(x) = exp(x^2);
+
+xf = zeros(5,2);
+xf(:,1) = 0.6:0.1:1;
+for i = 1:5
+    xf(i,2) = f(xf(i,1));
+end
+
+vandermondef = vandermonde(xf);
+lagrangef = Lagrange_Interpolation(xf);
+
+% Test ¢ò: f(x) = 1/(1+12*x^2), x ¡Ê[-1,1]; 15 equally spaced points:
+syms x;
+g(x) = 1/(1+12*x^2);
+
+xg = zeros(15,2);
+xg(:,1) = -1:1/7:1;
+for i = 1:15
+    xg(i,2) = g(xg(i,1));
+end
+
+vandermondeg = vandermonde(xg);
+lagrangeg = Lagrange_Interpolation(xg);
+
+% Test ¢ó£ºData points are given specifically:
+xd = [1994 67.501; 1995 68.008; 1996 69.803; 1997 72.024; 1998 73.400; 1999 72.063; 2000 74.669; 2001 74.487; 2002 74.065; 2003 76.777];
+
+vandermonded = vandermonde(xd);
+lagranged = Lagrange_Interpolation(xd);
+
+% ============================================
+
+% Plot of Vandermonde:
+subplot(2,3,1);
+hold on;
+title('Vandermonde polynomial');
+xlabel('x');
+ylabel('y');
+%plot(xf(:,1), xf(:,2), 'k.');
+%fplot(vandermondef,[0.6,1],'b');
+%plot(xg(:,1), xg(:,2), 'k*');
+%fplot(vandermondeg,[-1,1],'r');
+plot(xd(:,1), xd(:,2), 'k+');
+fplot(vandermonded,[1994,2003],'g');
+
+% Plot of Lagrange 
+subplot(2,3,3);
+hold on;
+title('Lagrange polynomial');
+xlabel('x');
+ylabel('y');
+plot(xf(:,1), xf(:,2), 'k.');
+fplot(lagrangef,[0.6,1],'b');
+plot(xg(:,1), xg(:,2), 'k*');
+fplot(lagrangeg,[-1,1],'r');
+%plot(xd(:,1), xd(:,2), 'k+');
+%fplot(lagranged,[1994,2003],'g');
+
+>>>>>>> Stashed changes
