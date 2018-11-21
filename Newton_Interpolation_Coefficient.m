39920d877a2f7cc275fe6feb63867f628318d261
Varun Chadha <vchadha023@gmail.com>
2018-11-21 08:37:37 -0600

    Fixed bug with alogorithm using index values instead of time values

--- Newton_Interpolation_Coefficient.m
+++ Newton_Interpolation_Coefficient.m
@@ -6,6 +6,8 @@ function [fc, data_new] = Newton_Interpolation_Coefficient(x, indecies, data)
 % Check base case -> return data at index
 % Recusrsive call
 
+
+
 l = length(indecies);
 if l == 1
     fc = x(indecies, 2);
@@ -16,5 +18,5 @@ end
 f_lower = Newton_Interpolation_Coefficient(x, indecies(1:end-1), data);
 f_upper = Newton_Interpolation_Coefficient(x, indecies(2:end), data);
 
-fc = (f_upper - f_lower) / (indecies(end) - indecies(1));
+fc = (f_upper - f_lower) / (x(indecies(end),1) - x(indecies(1),1));
 data_new = data;
