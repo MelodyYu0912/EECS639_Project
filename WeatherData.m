0d9c5904848e988936afdf906e9cd01da0ab5c8c
Varun Chadha <vchadha023@gmail.com>
2018-12-01 10:33:44 -0600

    Updated weather data script to use spline interpolation

--- WeatherData.m
+++ WeatherData.m
@@ -3,9 +3,13 @@
 clear, clc;
 
 %% Read data
+% Define file name containing data
 fileName = 'LawrenceWeatherData.csv';
+% Read csv data table
 weather_data = readtable(fileName);
 
+% Extract column information for temperature, precipitation, and dates of
+% data
 max_temp = table2array(weather_data(:, 7));
 max_temp = cellfun(@str2double, max_temp);
 
@@ -14,44 +18,110 @@ precipitation = cellfun(@str2double, precipitation);
 
 dates = table2array(weather_data(:, 3));
 
+% Define data increment variables
 st = 0;
 ed = 100;
 inc = 5;
 
+% Define dates to use on graphs
 datesToDisplay = datestr(dates(1:inc:end));
 
 %% Max temp
+% Create data to send to interpolating functions
 x = st:inc:ed;
 y = max_temp(x + 1);
 f = [ x' y ];
 
+% Retrieve Newton and Spline interpolation functions
 temp_newton = Newton_Interpolation(f);
+temp_natural_spline = Spline_Interpolation("natural", f);
+
+% Plot Newton interpolation
 figure;
-plot(x, y, 'ko');
+% Plot all temperature data points as black circles
+plot(st:ed, max_temp, 'ko');
 hold on;
-fplot(temp_newton, [st, ed]);
+% Plot interpolated data points as red circles
+plot(x, y, 'ro');
+% Plot interpolated function
+fplot(temp_newton, [ st, ed ]);
 hold off;
-title("Max temperature for Lawrence, KS");
+
+% Define axis labels and title
+title("Max temperature for Lawrence, KS with Newton Interpolation");
 xlabel('date');
 xticks(x);
 xticklabels(datesToDisplay);
 xtickangle(45);
-ylabel('temp, C');
+ylabel('temp (C)');
+
+% Plot Natural Cubic Spline Interpolation
+figure;
+% Plot all temperature data points as black circles
+plot(st:ed, max_temp, 'ko');
+hold on;
+% Plot interpolated data points as red circles
+plot(x, y, 'ro');
+% Plot interpolated piecwise functions
+for s = 1:length(temp_natural_spline)
+   fplot( temp_natural_spline(s), [ x(s), x(s + 1) ] );
+end
+hold off;
+
+% Define axis labels and title
+title("Max temperature for Lawrence, KS with Natural Cubic Spline Interpolation");
+xlabel('date');
+xticks(x);
+xticklabels(datesToDisplay);
+xtickangle(45);
+ylabel('temp (C)');
 
 %% Rainfall
+% Create data to send to interpolating functions
 x = st:inc:ed;
 y = precipitation(x + 1);
 f = [ x' y ];
 
+% Retrieve Newton and Spline interpolation functions
 precip_newton = Newton_Interpolation(f);
+precip_natrual_spline = Spline_Interpolation("natural", f);
+
+% Plot Newton interpolation
 figure;
-plot(x, y, 'ko');
+% Plot all precipitation data points as black circles
+plot(st:ed, precipitation, 'ko');
 hold on;
-fplot(precip_newton, [st, ed]);
+% Plot interpolated data points as red circles
+plot(x, y, 'ro');
+% Plot interpolated function
+fplot(precip_newton, [ st, ed ]);
 hold off;
+
+% Define axis labels and title
 title("Max precipitation for Lawrence, KS");
 xlabel('date');
 xticks(x);
 xticklabels(datesToDisplay);
 xtickangle(45);
-ylabel('precipitation');
+ylabel('precipitation (mm)');
+
+% Plot Natural Cubic Spline Interpolation
+figure;
+% Plot all precipitation data points as black circles
+plot(st:ed, precipitation, 'ko');
+hold on;
+% Plot interpolated data points as red circles
+plot(x, y, 'ro');
+% Plot interpolated piecwise functions
+for s = 1:length(precip_natrual_spline)
+   fplot( precip_natrual_spline(s), [ x(s), x(s + 1) ] );
+end
+hold off;
+
+% Define axis labels and title
+title("Max precipitation for Lawrence, KS with Natural Cubic Spline Interpolation");
+xlabel('date');
+xticks(x);
+xticklabels(datesToDisplay);
+xtickangle(45);
+ylabel('precipitation (mm)');
