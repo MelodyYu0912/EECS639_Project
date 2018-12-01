7a282211ca4ab824ea50837760407e8d18e3ad9b
Varun Chadha <vchadha023@gmail.com>
2018-11-30 18:45:09 -0600

    Added Guassian elimiation with parital pivoting function to project and updated Vandermonde interpolation to use function

---
+++ Guassian_Elimination_Partial_Pivot.m
@@ -0,0 +1,48 @@
+function [x] = Guassian_Elimination_Partial_Pivot (A, b)
+% GUASSIAN_ELIMINATION_PARTIAL_PIVOT   return GEEP solution of Ax = b
+%                                      with back substitution
+%                                      where A is n by n matrix and
+%                                      b is n by 1 matrix
+
+% Get length of input matrix
+n = length(A);
+% Create empty solutions vector
+x = zeros(n, 1);
+
+% Loop through each column of A
+for s = 1:n
+    % Find max element in column s and its corresponding index
+    [~, index] = max(A(s:end,s));
+    index = index + (s - 1);
+    
+    % If max is not at position (s, s), swap rows for A and b
+    if index ~= s
+        % Store temporary values so values overwriten are not lost
+        tempRow = A(s, :);
+        tempB = b(s);
+        
+        % Swap rows in A
+        A(s, :) = A(index, :);
+        A(index, :) = tempRow;
+        
+        % Swap elements in b
+        b(s) = b(index);
+        b(index) = tempB;
+    end
+    
+    % Perform Guassian Elimination and zero out rows below pivot
+    for t = (s + 1):n
+        % Create constant to multiply pivot row by
+        a = A(t, s) / A(s, s);
+        
+        % Update row below pivot
+        A(t, :) = A(t, :) - a * A(s, :);
+        b(t) = b(t) - a * b(s);
+    end
+end
+
+% Perform backwards substitution to solve for x
+x(end) = b(end) / A(end, end);
+for u = (n - 1):(-1):1
+    x(u) = (b(u) - sum(A(u, (u + 1):end) * x((u + 1):end))) / A(u, u);
+end
