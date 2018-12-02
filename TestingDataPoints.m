91f9c7124db21879ba3b663603bc97fa1cd141f0
Varun Chadha <vchadha023@gmail.com>
2018-12-01 19:15:33 -0600

    Added calls to spline interpolation for testing sample data

--- TestingDataPoints.m
+++ TestingDataPoints.m
@@ -15,6 +15,9 @@ x = [ t' f' ];
 f1_vandermonde = Vandermonde_Interpolation(x);
 f1_lagrange = Lagrange_Interpolation(x);
 f1_newton = Newton_Interpolation(x);
+f1_natural_spline = Spline_Interpolation("natural", x);
+f1_complete_spline = Spline_Interpolation("complete", x);
+f1_knot_spline = Spline_Interpolation("not-a-knot", x);
 
 figure;
 subplot(231);
@@ -44,6 +47,39 @@ title('Newton polynomial for f(x) = exp(x^2)');
 xlabel('x');
 ylabel('y');
 
+subplot(234);
+plot(t, f, 'ko');
+hold on;
+for s = 1:length(f1_natural_spline)
+   fplot( f1_natural_spline(s), [ t(s), t(s + 1) ] );
+end
+hold off;
+title('Natural Spline interpolation for f(x) = exp(x^2)');
+xlabel('x');
+ylabel('y');
+
+subplot(235);
+plot(t, f, 'ko');
+hold on;
+for s = 1:length(f1_complete_spline)
+   fplot( f1_complete_spline(s), [ t(s), t(s + 1) ] );
+end
+hold off;
+title('Complete Spline interpolation for f(x) = exp(x^2)');
+xlabel('x');
+ylabel('y');
+
+subplot(236);
+plot(t, f, 'ko');
+hold on;
+for s = 1:length(f1_knot_spline)
+   fplot( f1_knot_spline(s), [ t(s), t(s + 1) ] );
+end
+hold off;
+title('Not-a-knot Spline interpolation for f(x) = exp(x^2)');
+xlabel('x');
+ylabel('y');
+
 %% Test ¢ò: f(x) = 1/(1+12*x^2), x ¡Ê[-1,1]; 15 equally spaced points:
 st = -1;
 et = 1;
@@ -58,6 +94,9 @@ x = [ t' f' ];
 f2_vandermonde = Vandermonde_Interpolation(x);
 f2_lagrange = Lagrange_Interpolation(x);
 f2_newton = Newton_Interpolation(x);
+f2_natural_spline = Spline_Interpolation("natural", x);
+f2_complete_spline = Spline_Interpolation("complete", x);
+f2_knot_spline = Spline_Interpolation("not-a-knot", x);
 
 figure;
 subplot(231);
@@ -87,6 +126,39 @@ title('Newton polynomial for f(x) = 1/(1+12*x^2)');
 xlabel('x');
 ylabel('y');
 
+subplot(234);
+plot(t, f, 'ko');
+hold on;
+for s = 1:length(f2_natural_spline)
+   fplot( f2_natural_spline(s), [ t(s), t(s + 1) ] );
+end
+hold off;
+title('Natural Spline interpolation for f(x) = 1/(1+12*x^2)');
+xlabel('x');
+ylabel('y');
+
+subplot(235);
+plot(t, f, 'ko');
+hold on;
+for s = 1:length(f2_complete_spline)
+   fplot( f2_complete_spline(s), [ t(s), t(s + 1) ] );
+end
+hold off;
+title('Complete Spline interpolation for f(x) = 1/(1+12*x^2)');
+xlabel('x');
+ylabel('y');
+
+subplot(236);
+plot(t, f, 'ko');
+hold on;
+for s = 1:length(f2_knot_spline)
+   fplot( f2_knot_spline(s), [ t(s), t(s + 1) ] );
+end
+hold off;
+title('Not-a-knot Spline interpolation for f(x) = 1/(1+12*x^2)');
+xlabel('x');
+ylabel('y');
+
 %% Test ¢ó£ºData points are given specifically:
 st = 1994;
 et = 2003;
@@ -99,6 +171,9 @@ x = [ 1994 67.501; 1995 68.008;
 f3_vandermonde = Vandermonde_Interpolation(x);
 f3_lagrange = Lagrange_Interpolation(x);
 f3_newton = Newton_Interpolation(x);
+f3_natural_spline = Spline_Interpolation("natural", x);
+f3_complete_spline = Spline_Interpolation("complete", x);
+f3_knot_spline = Spline_Interpolation("not-a-knot", x);
 
 figure;
 subplot(231);
@@ -125,5 +200,38 @@ hold on;
 fplot(f3_newton, [st,et], 'r');
 hold off;
 title('Newton polynomial for f3');
+xlabel('x');
+ylabel('y');
+
+subplot(234);
+plot(t, f, 'ko');
+hold on;
+for s = 1:length(f3_natural_spline)
+   fplot( f3_natural_spline(s), [ x(s, 1), x(s + 1, 1) ] );
+end
+hold off;
+title('Natural Spline interpolation for f3');
+xlabel('x');
+ylabel('y');
+
+subplot(235);
+plot(t, f, 'ko');
+hold on;
+for s = 1:length(f3_complete_spline)
+   fplot( f3_complete_spline(s), [ x(s, 1), x(s + 1, 1) ] );
+end
+hold off;
+title('Complete Spline interpolation for f3');
+xlabel('x');
+ylabel('y');
+
+subplot(236);
+plot(t, f, 'ko');
+hold on;
+for s = 1:length(f3_knot_spline)
+   fplot( f3_knot_spline(s), [ x(s, 1), x(s + 1, 1) ] );
+end
+hold off;
+title('Not-a-knot Spline interpolation for f3');
 xlabel('x');
 ylabel('y');
